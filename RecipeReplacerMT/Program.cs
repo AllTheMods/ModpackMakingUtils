7f46635b318a4b4d2f9ac23794ab7a649f14672c
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2017-04-05 23:35:07 +0200

    added support for config file, fixed stuff

--- RecipeReplacerMT/Program.cs
+++ RecipeReplacerMT/Program.cs
@@ -11,9 +11,9 @@ namespace RecipeReplacerMT
     {
 
         static List<string> AllRecipes = new List<string>();
-        static List<string> RecipesToChange = new List<string>();
-        static List<string> ChangedRecipes = new List<string>();
-        static List<string> RemovedRecipes = new List<string>();
+
+        // static List<string> ChangedRecipes = new List<string>();
+        // static List<string> RemovedRecipes = new List<string>();
 
 
         static List<replaceWithStruct> replaceList;
@@ -23,10 +23,11 @@ namespace RecipeReplacerMT
         {
             string filepath = @"C:\Users\jonas\Documents\Minecraft\Instances\All the Mods (1)\minetweaker.log";
 
-
-            if (false)
+            // enabled path input
+            if (true)
             {
                 Console.WriteLine("Insert Filepath");
+
                 filepath = Console.ReadLine();
                 if (filepath.Last() == '"')
                 {
@@ -38,27 +39,41 @@ namespace RecipeReplacerMT
                     filepath = filepath.Remove(0, 1);
                 }
             }
-            readText(filepath);
+
+            readRecipesFromFile(filepath);
             initReplaceList();
+            
+            // only runs when there really is something to do
+            if (replaceList.Count > 0)
+            {
+                // gets all recipes that gotta be changed
+                List<string> RecipesToChange = searchRecipesToChange();
+                Console.WriteLine("\nNormal Recipes: ");
+                printRecipes(RecipesToChange);
 
+                // gets the recipes changed and printed
+                Console.WriteLine("\nChanged Recipes:");
+                List<string> ChangedRecipes = changeRecipes(RecipesToChange);
+                printRecipes(ChangedRecipes);
 
-            searchRecipesToChange();
-            Console.WriteLine("\nNormal Recipes: ");
-            printRecipes(RecipesToChange);
+                // gets all the removed recipes
+                List<string> RemovedRecipes = createRemoveRecipes(RecipesToChange, ref ChangedRecipes);
 
-            Console.WriteLine("\nChanged Recipes:");
-            changeRecipes();
-            printRecipes(ChangedRecipes);
-            createRemoveRecipes();
-            writeToFile("test.zs", RemovedRecipes, ChangedRecipes);
+                //fixed both lists
+                fixList(ref RemovedRecipes);
+                fixList(ref ChangedRecipes);
+
+                writeToFile("test.zs", RemovedRecipes, ChangedRecipes);
+            }
+
 
 
 
             Console.ReadLine();
         }
 
-
-        static void readText(string source)
+        // reads all recipes from the file
+        static void readRecipesFromFile(string source)
         {
             FileStream fs = null;
             AllRecipes.Clear();
@@ -71,7 +86,12 @@ namespace RecipeReplacerMT
                     string line;
                     while ((line = file.ReadLine()) != null)
                     {
-                        AllRecipes.Add(line);
+                        // only takes the actual recipes
+                        if (line.StartsWith("[SERVER_STARTED][SERVER]recipes.add"))
+                        {
+                            AllRecipes.Add(line.Substring(24));
+                        }
+
                     }
 
                     Console.WriteLine("\nThe file had {0} lines.", AllRecipes.Count);
@@ -87,13 +107,19 @@ namespace RecipeReplacerMT
 
         }
 
+        // Outputs the given list into a file, lists separated by newline
         static void writeToFile(string path, params List<string>[] lists)
         {
             FileStream fs = null;
-
             try
             {
-                
+                if (File.Exists(path))
+                {
+                    if (File.Exists(path.Replace(".zs", "_old.zs"))){
+                        File.Delete(path.Replace(".zs", "_old.zs"));
+                    }
+                    File.Move(path, path.Replace(".zs", "_old.zs"));
+                }
 
                 fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
                 using (StreamWriter file = new StreamWriter(fs))
@@ -120,35 +146,98 @@ namespace RecipeReplacerMT
             }
         }
 
+        // inits all the recipes that are supposed to be replaced
+        //TODO: Replace with read from config
         static void initReplaceList()
         {
             replaceList = new List<replaceWithStruct>();
+
+            FileStream fs = null;
+
+            try
+            {
+                // loads the config file
+                fs = new FileStream(Environment.CurrentDirectory + "\\recipes.cfg", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                using (StreamReader file = new StreamReader(fs))
+                {
+                    string line;
+                    
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        // only takes the actual recipes
+                        if (!line.StartsWith("#"))
+                        {
+                            string[] split = line.Split('>');
+                            replaceList.Add(new replaceWithStruct("<" + split[0].Trim() + ">", "<" + split[1].Trim() + ">" ));
+                        }
+
+                    }
+                    // checks if there is something in the file, otherwise adds something to it
+                    if (replaceList.Count < 1)
+                    {
+                        Console.WriteLine(@"Replace List is empty, please add entries to the file. Stlye: [minecraft:iron_ingot>ore:ingotIron]. One entry per line");
+                        if (fs.Length < 1)
+                        {
+                            using (StreamWriter writer = new StreamWriter(fs))
+                            {
+                                writer.WriteLine("#one entry per line");
+                                writer.WriteLine("#minecraft:iron_ingot>ore:ingotIron");
+                            }
+                        }
+
+                    }else
+                    {
+                        Console.WriteLine("\nTrying to replace {0} entries.\nThe following entries are getting replaced", replaceList.Count);
+                        foreach (var l in replaceList)
+                        {
+                            Console.WriteLine(l.ToString());
+                        }
+                    }
+
+                }
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Dispose();
+                }
+            }
+
+
+            /*
             replaceList.Add(new replaceWithStruct("<railcraft:plate:1>", "<ore:plateSteel>"));
             replaceList.Add(new replaceWithStruct("<railcraft:plate>", "<ore:plateIron>"));
             replaceList.Add(new replaceWithStruct("<railcraft:plate:4>", "<ore:plateLead>"));
-
-
+            replaceList.Add(new replaceWithStruct("<techreborn:techreborn.machineFrame>", "<ore:machineBlockBasic>"));
+            replaceList.Add(new replaceWithStruct("<ic2:resource:12>", "<ore:machineBlockBasic>"));
+            */
         }
 
-        static void searchRecipesToChange()
+        // goes through all the recipes and looks what has to be changed
+        // returns list of elements that got to be changed
+        static List<string> searchRecipesToChange()
         {
-            foreach(string line in AllRecipes)
+            List<string> toChangeList = new List<string>();
+
+            for (int i = 0; i < AllRecipes.Count; i++)
             {
                 foreach(replaceWithStruct r in replaceList)
                 {
-                    string s = line.Replace("[SERVER_STARTED][SERVER]", "");
+                    string s = AllRecipes[i];
+
                     if (s.Contains(r.origin) && !(s.StartsWith("recipes.addShaped(" + r.origin) || s.StartsWith("recipes.addShapeless(" + r.origin)))
                     {
-
-                        RecipesToChange.Add(s);
+                        toChangeList.Add(s);
+                        AllRecipes[i] = "{This element got replaced}";
                         break;
                     }
                 }
-
-
             }
+            return toChangeList;
         }
 
+        // prints out the given list 
         static void printRecipes(List<string> list)
         {
             foreach(string l in list)
@@ -157,24 +246,57 @@ namespace RecipeReplacerMT
             }
         }
 
-        static void changeRecipes()
+        // changes all entries in that list and gives back a new, changed list
+        static List<string> changeRecipes(List<string> recipesToChange)
         {
-            foreach(string r in RecipesToChange)
+            List<string> changedRecipes = new List<string>();
+
+            foreach(string r in recipesToChange)
             {
                 string changeMe = r;
-                foreach(replaceWithStruct s in replaceList)
+
+                foreach (replaceWithStruct s in replaceList)
                 {
                     changeMe = changeMe.Replace(s.origin, s.replaceWith);
                 }
-                ChangedRecipes.Add(changeMe);
+                changedRecipes.Add(changeMe);
             }
+
+            return changedRecipes;
         }
 
-        static void createRemoveRecipes()
+        // creates the list of code to remove it
+        static List<string> createRemoveRecipes(List<string> recipesToChange, ref List<string> changedRecipes)
         {
-            foreach (var recipe in RecipesToChange)
+            List<string> removedRecipes = new List<string>();
+
+            foreach (var recipe in recipesToChange)
             {
-                RemovedRecipes.Add(recipe.Replace("add", "remove"));
+                // checks whether the are other crafting recipes for the thing, if yes, then adds it back in
+                string substring = recipe.Substring(0, recipe.IndexOf(">") + 1);
+
+                Console.WriteLine("substring: " + substring);
+                foreach(string line in AllRecipes)
+                {
+                    if (line.Contains(substring) && !recipesToChange.Contains(line) && !changedRecipes.Contains(line))
+                    {
+                        changedRecipes.Add(line);
+                    }
+                }
+                removedRecipes.Add(recipe.Replace("add", "remove"));
+
+            }
+
+            return removedRecipes;
+        }
+
+        // fix list 
+        static void fixList(ref List<string> brokenList)
+        {
+            for (int i = 0; i < brokenList.Count; i++)
+            {
+                brokenList[i] = brokenList[i].Replace(".withTag({})", "");
+
             }
         }
     }
@@ -187,6 +309,10 @@ namespace RecipeReplacerMT
         {
             this.origin = origin;
             this.replaceWith = replaceWith;
+        }
+        public override string ToString()
+        {
+            return origin + " --> " + replaceWith;
         }
 
         public string origin;
